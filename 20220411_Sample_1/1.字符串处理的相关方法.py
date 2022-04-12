87ec5006aa60149f2783a1f60810d42c5201829a
SuperRed7 <798500814@qq.com>
2022-04-12 15:10:04 +0800

    20220412-补充字符串处理的相关方法

--- 20220411_Sample_1/1.字符串处理的相关方法.py
+++ 20220411_Sample_1/1.字符串处理的相关方法.py
@@ -5,7 +5,7 @@ value_2=value_1.lower()
 value_3=value_1.upper()
 print(value_1,value_2,value_3)
 
-# 字符串的分隔
+# 字符串的分隔,返回一个列表
 value_4='123@qq.com'
 list_1=value_4.split('@')
 print(list_1,type(list_1))
@@ -28,3 +28,28 @@ print(value_1.startswith('P'))
 
 print('sample.py'.endswith('.py'))
 print('sample.txt'.endswith('.py'))
+
+# 字符串的替换
+value_5=value_1.replace('o','你好')
+print(value_1,value_5)
+
+# 字符串在指定的宽度范围内居中
+print(value_1.center(20))
+print(value_1.center(20,'*'))
+
+# 去除字符串左右的空格
+value_6='    Hello World    '
+print(value_6.strip())
+# 去除字符串左侧的空格
+print(value_6.lstrip())
+# 去除字符串右侧的空格
+print(value_6.rstrip())
+
+# 去除指定的字符
+value_7='dl-HelloWorld'
+# 注意:不管dl或ld,只要左右包含l或d,就去除
+print(value_7.strip('ld'))
+# 去除左侧指定的字符
+print(value_7.lstrip('ld'))
+# 去除右侧指定的字符
+print(value_7.rstrip('ld'))
