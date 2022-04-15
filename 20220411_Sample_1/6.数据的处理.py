440c54bd009ad3f04f433f1e341eaa0aa09f3fc4
SuperRed7 <798500814@qq.com>
2022-04-15 18:12:19 +0800

    20220415-增加列表元素的去重

--- 20220411_Sample_1/6.数据的处理.py
+++ 20220411_Sample_1/6.数据的处理.py
@@ -29,9 +29,28 @@ for i in range(len(value_3)):
     if value_3[i] not in value_4:
         value_4 += value_3[i]
 print(value_4)
-# 3.通过集合去重+列表的排序
+# 3.通过集合去重及列表的排序
 value_4 = set(value_3)
 list_1 = list(value_4)
 # 排序的关键字
 list_1.sort(key=value_3.index)
 print(''.join(list_1))
+
+# 列表元素的去重
+list_2 = ['金星', '木星', '水星', '火星', '金星', '木星', '水星', '火星']
+list_3 = []
+# 1.遍历及not in
+for i in list_2:
+    if i not in list_3:
+        list_3.append(i)
+print(list_3)
+# 2.使用索引及not in
+for i in range(len(list_2)):
+    if list_2[i] not in list_3:
+        list_3.append(list_2[i])
+print(list_3)
+# 3.通过集合去重及列表的排序
+value_5 = set(list_2)
+list_4 = list(value_5)
+list_4.sort(key=list_2.index)
+print(list_4)
