85d8353dde00ca8a9ec29bd50cd7f0a6cb92fb8d
SuperRed7 <798500814@qq.com>
2022-04-21 03:50:08 +0800

    20220421-斐波那契数列(格式化文件以保持美观)

--- 20220420_Sample_1/3.斐波那契数列.py
+++ 20220420_Sample_1/3.斐波那契数列.py
@@ -1,22 +1,24 @@
 # coding:utf-8
 def fun(n):
-    if n==1:
+    if n == 1:
         return 1
-    elif n==2:
+    elif n == 2:
         return 1
     else:
-        result=fun(n-1)+fun(n-2)
+        result = fun(n - 1) + fun(n - 2)
         return result
+
+
 print('-----输出斐波那契数列第6位上的数字-----')
 print(fun(6))
 
 print('-----1.输出斐波那契数列前6位上的数字-----')
-for i in range(1,7):
+for i in range(1, 7):
     print(fun(i))
 
-i=1
-n=eval(input('请输入一个整数:\t'))
-print('-----2.输出斐波那契数列前'+str(n)+'位上的数字-----')
-while i<n+1:
+i = 1
+n = eval(input('请输入一个整数:\t'))
+print('-----2.输出斐波那契数列前' + str(n) + '位上的数字-----')
+while i < n + 1:
     print(fun(i))
-    i+=1
+    i += 1
