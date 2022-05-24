47972c3debafcf17c7781b6526e50f6a1160c049
SuperRed7 <798500814@qq.com>
2022-05-24 18:04:23 +0800

    20220524-异常处理机制1:try-except

---
+++ 20220524_Sample_1/1.try-except.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+try:
+    a = int(input('请输入第一个整数:'))
+    b = int(input('请输入第二个整数:'))
+    result = a / b
+    print('结果为:', result)
+except ZeroDivisionError:
+    print('除数不允许为0')
+except ValueError:
+    print('只能输入字符串')
+print('程序结束')
